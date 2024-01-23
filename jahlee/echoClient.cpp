1c612d4f7bfcf0b3ec9319534f9155a7824cff40
이자훈 <jahlee@student.42seoul.kr>
2024-01-23 13:01:24 +0900

    :sparkles: feat: 컴파일 파일 추가 및 클라이언트 헤더 수정

--- jahlee/echoClient.cpp
+++ jahlee/echoClient.cpp
@@ -1,14 +1,8 @@
-#include <sys/types.h>
-#include <sys/event.h>
-#include <sys/time.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
-#include <fcntl.h>
 
 #include <iostream>
-#include <map>
-#include <vector>
 
 #define BUF_SIZE 1024
 using namespace std;
