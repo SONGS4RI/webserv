3e0a02a0475f4d9946956d4bcf7ff10a3383dc2c
이자훈 <jahlee@student.42seoul.kr>
2024-02-28 15:59:10 +0900

    :sparkles: feat: ResponseBody 추가

--- modules/ResponseBody.cpp
+++ modules/ResponseBody.cpp
@@ -1,5 +1,44 @@
 #include "ResponseBody.hpp"
 
+ResponseBody::ResponseBody() {}
+
+ResponseBody::~ResponseBody() {}
+
+const size_t& ResponseBody::getStatusCode() const {
+	return statusCode;
+}
+const string& ResponseBody::getContentType() const {
+	return contentType;
+}
+
+const size_t& ResponseBody::getContentLength() const {
+	return contentLength;
+}
+
+const string& ResponseBody::getBody() const {
+	return body;
+}
+
+void ResponseBody::setStatusCode(const size_t& statusCode) {
+	this->statusCode = statusCode;
+}
+
+void ResponseBody::setContentType(const string& contentType) {
+	this->contentType = contentType;
+}
+
+void ResponseBody::setContentLength(const size_t& contentLength) {
+	this->contentLength = contentLength;
+}
+
+void ResponseBody::setBody(const char* str, const size_t& readCnt) {
+	this->body.resize(readCnt);
+	for (size_t i = 0; i < readCnt; i++) {
+		this->body[i] = str[i];
+	}
+}
+#include "ResponseBody.hpp"
+
 ResponseBody::ResponseBody(size_t code, string _type, string _body) {
 	statusCode = code;
 	contentType = _type;
--- modules/ResponseBody.hpp
+++ modules/ResponseBody.hpp
@@ -32,4 +32,46 @@ class ResponseBody {
 		// void addBody(const char* str, const size_t& readCnt);
 };
 
+#endif#pragma once
+#ifndef RESPONSEBODY_HPP
+#define RESPONSEBODY_HPP
+#include <string>
+#include "./enum/Enums.hpp"
+using namespace std;
+class ResponseBody {
+    private:
+        size_t statusCode;
+        string contentType;
+        size_t contentLength;
+        string body;
+    public:
+        ResponseBody();
+        ~ResponseBody();
+        ResponseBody(const ResponseBody& obj);
+        const ResponseBody& operator=(const ResponseBody& obj);
+
+        const size_t& getStatusCode() const;
+        const string& getContentType() const;
+        const size_t& getContentLength() const;
+        const string& getBody() const;
+
+        void setStatusCode(const size_t& statusCode);
+        void setContentType(const string& contentType);
+        void setContentLength(const size_t& contentLength);
+        void setBody(const char* str, const size_t& readCnt);
+};
 #endif
+
+
+/*
+
+메서드 관련 405
+
+GET 200 -> body
+    30X -> 404 html 리다이렉션
+POST 201 -> body X
+    400 -> 잘못된 url
+DELETE 204 -> body X
+    400 -> 잘못된 url
+
+*/
