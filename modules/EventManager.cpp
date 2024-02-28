007b7600881326f8b30ade9951f9c9fe2d55ffb8
이자훈 <jahlee@student.42seoul.kr>
2024-02-28 15:46:16 +0900

    :hammer: refactor: EventManager 수정

--- modules/EventManager.cpp
+++ modules/EventManager.cpp
@@ -19,11 +19,16 @@ int EventManager::detectEvent() {
 	kevent(kq, &change_list[0], change_list.size(), event_list, EVENT_MAX_CNT, NULL);
 }
 
-void EventManager::addEvents(vector<struct kevent>& change_list, uintptr_t ident, int16_t filter,
-					uint16_t flags, uint32_t fflags, intptr_t data, void* udata) {
+void EventManager::addEvent(uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags,
+							intptr_t data, void* udata) {
 	struct kevent tmp_event;
-	EV_SET(&tmp_event, ident, filter, flags, fflags, data, udata);// kevent 구조체 초기화 매크로
+	EV_SET(&tmp_event, ident, filter, flags, fflags, data, udata);
 	change_list.push_back(tmp_event);
+}
+
+void EventManager::changeEvent(struct kevent* curEvent, int16_t filter, uint16_t flags,
+								uint32_t fflags, intptr_t data, void* udata) {
+	EV_SET(curEvent, curEvent->ident, filter, flags, fflags, data, udata);
 }
 
 void EventManager::handleEvent(const int& eventIdx) {
@@ -36,23 +41,26 @@ void EventManager::handleEvent(const int& eventIdx) {
 			exit(1);
 		}
 		cerr << "client socket error" << endl;
-		sm->disconnect_client(curEvent->ident);
+		sm->disconnectClient(curEvent->ident);
 	} else if (curEvent->flags ==  EVFILT_READ) {// 읽기
 		if (sm->isServerSocket(curEvent->ident)) {
+			// 새로운 클라이언트 등록 및 read 이벤트 생성
 			// sm->clients.push_back(Client());
+			int clientSocket = sm->acceptClient(curEvent->ident);
+			addEvent(clientSocket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
 		} else if (curEvent->udata == NULL) {
 			Client client = sm->getClient(curEvent->ident);
 			// 클라이언트의 처리 상태에 따라 이벤트 처리
 			Request* request = client.getCurReqeust();
 			request->parseRequest(client);
 			if (request->getStatus() == PARSE_DONE) {
-				addEvents(change_list, curEvent->ident, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, request);
+				changeEvent(curEvent, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, request);
 			}
 		} else {
 			Request* request = (Request *)curEvent->udata;
-			RequestHandler requestHandler = RequestHandler(*request);
-			// Response* response = &(hr->handleRequest());
-			// addEvents(change_list, curEvent->ident, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, response);
+			RequestHandler requestHandler = RequestHandler(request);
+			ResponseBody* responseBody = requestHandler.handleRequest();
+			changeEvent(curEvent, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, responseBody);
 		}
 	} else if (curEvent->flags ==  EVFILT_WRITE) {// 쓰기
 		/*
