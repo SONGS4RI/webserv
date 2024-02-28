7247576362668247a8f54a47041b24178590565e
이자훈 <jahlee@student.42seoul.kr>
2024-02-28 16:04:57 +0900

    :hammer: refactor: ResponseBody 수정

--- modules/ResponseBody.cpp
+++ modules/ResponseBody.cpp
@@ -37,27 +37,3 @@ void ResponseBody::setBody(const char* str, const size_t& readCnt) {
 		this->body[i] = str[i];
 	}
 }
-#include "ResponseBody.hpp"
-
-ResponseBody::ResponseBody(size_t code, string _type, string _body) {
-	statusCode = code;
-	contentType = _type;
-	contentLength = _body.size();
-	body = _body;
-}
-
-const size_t& ResponseBody::getStatusCode() const {
-	return (statusCode);
-}
-
-const string& ResponseBody::getContentType() const {
-	return (contentType);
-}
-
-const size_t& ResponseBody::getContentLength() const {
-	return (contentLength);
-}
-
-const string& ResponseBody::getBody() const {
-	return (body);
-}
--- modules/ResponseBody.hpp
+++ modules/ResponseBody.hpp
@@ -15,53 +15,23 @@ class ResponseBody {
 		size_t contentLength;
 		string body;
 	public:
-		//ResponseBody();
-		ResponseBody(size_t code, string _type, string _body);
-		//~ResponseBody();
-		//ResponseBody(const ResponseBody& obj);
-		// const ResponseBody& operator=(const ResponseBody& obj);
-		// void init(const string& contentType, const size_t& contentLength);
+		ResponseBody();
+		~ResponseBody();
+		ResponseBody(const ResponseBody& obj);
+		const ResponseBody& operator=(const ResponseBody& obj);
 
 		const size_t& getStatusCode() const;
 		const string& getContentType() const;
 		const size_t& getContentLength() const;
 		const string& getBody() const;
 
-		// void setContentLength(const size_t& contentLength);
-		// void setChunkedStatus(const EChunkedStatus& chunkedStatus);
-		// void addBody(const char* str, const size_t& readCnt);
-};
-
-#endif#pragma once
-#ifndef RESPONSEBODY_HPP
-#define RESPONSEBODY_HPP
-#include <string>
-#include "./enum/Enums.hpp"
-using namespace std;
-class ResponseBody {
-    private:
-        size_t statusCode;
-        string contentType;
-        size_t contentLength;
-        string body;
-    public:
-        ResponseBody();
-        ~ResponseBody();
-        ResponseBody(const ResponseBody& obj);
-        const ResponseBody& operator=(const ResponseBody& obj);
-
-        const size_t& getStatusCode() const;
-        const string& getContentType() const;
-        const size_t& getContentLength() const;
-        const string& getBody() const;
-
         void setStatusCode(const size_t& statusCode);
         void setContentType(const string& contentType);
         void setContentLength(const size_t& contentLength);
         void setBody(const char* str, const size_t& readCnt);
 };
-#endif
 
+# endif
 
 /*
 
