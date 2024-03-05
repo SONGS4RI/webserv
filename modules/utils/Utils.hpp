7fb9cecfb296d963136dcac9aa33dc5f8eafcc01
이자훈 <jahlee@student.42seoul.kr>
2024-03-05 11:00:38 +0900

    :sparkles: [Feature] 로깅 함수 세팅 (#31)

--- modules/utils/Utils.hpp
+++ modules/utils/Utils.hpp
@@ -6,6 +6,15 @@
 #include <cstdlib>
 #include <string>
 
+#define RESET "\033[0m"
+#define GRAY "\033[1;90m"
+#define RED "\033[1;91m"
+#define GREEN "\033[1;92m"
+#define YELLOW "\033[1;93m"
+#define BLUE "\033[1;94m"
+#define MAGENTA "\033[1;95m"
+#define CYAN "\033[1;96m"
+
 using namespace std;
 
 class Utils {
@@ -15,6 +24,7 @@ class Utils {
 		static void exitWithErrmsg(const string& errmsg);
 		static string intToString(const int& number);
 		static size_t hexToDecimal(const string& hex);
+		static void log(const string& msg, const string& color);
 };
 
 #endif
