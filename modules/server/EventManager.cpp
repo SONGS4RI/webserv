b5c74dca5bc3295ca857b83abc09ec0dcdc50304
이자훈 <jahlee@student.42seoul.kr>
2024-03-05 12:43:01 +0900

    :sparkles: [Feature]  자잘한 수정 + 로깅 문구 추가 (#34)

--- modules/server/EventManager.cpp
+++ modules/server/EventManager.cpp
@@ -19,6 +19,7 @@ void EventManager::init() {
 	if (kq == -1) {
 		Utils::exitWithErrmsg("Error: kqueue()");
 	}
+	change_list.reserve(EVENT_MAX_CNT);
 }
 
 EventManager* EventManager::getInstance() {
@@ -33,7 +34,12 @@ EventManager* EventManager::getInstance() {
 }
 
 int EventManager::detectEvent() {
-	return (kevent(kq, &change_list[0], change_list.size(), event_list, EVENT_MAX_CNT, NULL));
+	int newEvents = kevent(kq, &change_list[0], change_list.size(), event_list, EVENT_MAX_CNT, NULL);
+	if (newEvents < 0) {
+		Utils::exitWithErrmsg("Detect Event Error");
+	}
+	change_list.clear();
+	return (newEvents);
 }
 
 void EventManager::addEvent(uintptr_t ident, int16_t filter, uint16_t flags, uint32_t fflags,
@@ -52,12 +58,11 @@ void EventManager::handleEvent(const int& eventIdx) {
 	struct kevent* curEvent = &event_list[eventIdx];
 	SocketManager* sm = SocketManager::getInstance();
 
-	if (curEvent->filter & EV_ERROR) {// 에러
+	if (curEvent->flags & EV_ERROR) {// 에러
 		if (sm->isServerSocket(curEvent->ident)) {
-			// 서버 에러 발생하면 안되지만 일단 체크
-			exit(1);
+			Utils::exitWithErrmsg("Server Socket Error");
 		}
-		cerr << "client socket error" << endl;
+		Utils::log("client socket error", RED);
 		sm->disconnectClient(curEvent->ident);
 	} else if (curEvent->filter ==  EVFILT_READ) {// 읽기
 		Client* client = sm->getClient(curEvent->ident);
@@ -67,20 +72,25 @@ void EventManager::handleEvent(const int& eventIdx) {
 			try {
 				clientSocket = sm->acceptClient(curEvent->ident);
 				if (clientSocket == -1) { //accept()를 실패해서 클라이언트 소켓 할당도 안된 경우
+					Utils::log("accept() Failed", YELLOW);
 					return ;
 				}
 				sm->addClient(curEvent->ident, clientSocket);//throw errCode 발생 가능
 				addEvent(clientSocket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, NULL);
 			} catch (StatusCode& errCode) {
 				//accept는 성공했는데 다른 문제가 발생한 경우 -> 즉시 에러 리스폰스 만든다.
+				Utils::log("accept() Successed But Something went wrong", YELLOW);
 				Response*	errResponse = new Response(errCode);
 				addEvent(clientSocket, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, errResponse);
 			}
 		} else if (curEvent->udata == NULL) {
 			// 클라이언트의 처리 상태에 따라 이벤트 처리
+			Utils::log("Client: " + Utils::intToString(curEvent->ident) + ": Parsing", GREEN);
 			Request* request = client->getReqeust();// NULL 이면 할당해서 주기
 			request->parseRequest(*client);
 			if (request->getStatus() == PARSE_DONE || request->getStatusCode().getStatusCode() >= 400) {
+				Utils::log("Client: " + Utils::intToString(curEvent->ident) +
+				(request->getStatus() == PARSE_DONE ? "Parse Done" : "Parse Error"), GREEN);
 				changeEvent(curEvent, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, request);
 			}
 		} else {
@@ -88,6 +98,8 @@ void EventManager::handleEvent(const int& eventIdx) {
 			RequestHandler requestHandler = RequestHandler(request, client);
 			ResponseBody* responseBody = requestHandler.handleRequest();
 			if (responseBody != NULL) {// cgi 아니라면
+				Utils::log("Client: " + Utils::intToString(curEvent->ident) + ": Response Created: " +
+				responseBody->getStatusCode().getMessage(), GREEN);
 				Response* response = new Response(responseBody);
 				client->setResponse(response);
 				changeEvent(curEvent, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, response);
@@ -95,10 +107,13 @@ void EventManager::handleEvent(const int& eventIdx) {
 		}
 	} else if (curEvent->filter ==  EVFILT_WRITE) {// 쓰기
 		if (curEvent->udata != NULL) {//쓸게 있으면 buffsize만큼 쓰기
+			Utils::log("Client: " + Utils::intToString(curEvent->ident) + ": Writing", GREEN);
 			Response* response = (Response *)curEvent->udata;
 			response->writeToSocket(curEvent->ident);
 			if (response->isDone()) {
+				Utils::log("Client: " + Utils::intToString(curEvent->ident) + ": Write Done", GREEN);
 				sm->disconnectClient(curEvent->ident);
+				changeEvent(curEvent, EVFILT_READ, EV_DELETE, 0, 0, NULL);
 			}
 		}
 	}
